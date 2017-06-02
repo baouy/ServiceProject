d3bcd4046010c648d343b0f641f5fe105d28e655
DavidWangTM <232190315@qq.com>
2017-06-02 17:24:44 +0800

    加入swagger配置

---
+++ tudou-upms/tudou-upms-service/src/main/java/com/tudou/upms/server/swagger/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.tudou.upms.server.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 2016-6-2
+ * 
+ * @author ever Swagger 配置
+ */
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("美窝云")
+				.version("1.0")
+				.description("api-文档描述")
+				.build();
+	}
+
+}
